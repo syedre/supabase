cea3ef3d4700936db8ca16d336851d5907ff1ab5
syedre <syed.rehan123@gmail.com>
2026-06-16 12:06:57 +0530

    moved grid lines to right side

--- src/components/authLayout.jsx
+++ src/components/authLayout.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const AuthLayout = ({ children }) => {
   return (
     <div className="h-screen w-full grid grid-cols-2">
+      <div className="bg-white border-r  border-zinc-200"></div>
       <div className="grid grid-cols-4 grid-rows-4 gap-px bg-zinc-200 border-r border-zinc-200 relative">
         <div className="bg-transparent "></div>
         <div className="bg-white"></div>
