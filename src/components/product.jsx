1626e9f20fb74f313c96293adbdc745a60f0d3fb
syedre <syed.rehan123@gmail.com>
2026-06-17 19:59:23 +0530

    separated products componsnets

---
+++ src/components/product.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import prod_1 from "../assets/prod_1.jpeg";
+import prod_2 from "../assets/prod_2.jpeg";
+import prod_3 from "../assets/prod_3.jpeg";
+import prod_4 from "../assets/prod_4.jpeg";
+
+const ProductDisplay = () => {
+  const obj = {
+    1: prod_1,
+    2: prod_2,
+    3: prod_3,
+    4: prod_4,
+  };
+  return (
+    <>
+      <div className="p-10 h-full">
+        <h2 className=" text-2xl pb-2">Products</h2>
+        <div className="border border-primary-wooden w-full h-full rounded-2xl flex flex-col items-center gap-2 pb-4">
+          <div className="grid grid-cols-4 gap-2 w-full h-auto p-2 ">
+            {[1, 2, 3, 4]?.map((i) => {
+              const url = `prod_${i}`;
+              return (
+                <div className="product-card">
+                  <div className="flex-2">
+                    <img
+                      src={obj[i]}
+                      alt={`prod_${i}`}
+                      className="aspect-video w-full h-75 rounded-b-2xl"
+                    />
+                  </div>
+                  <div className="flex-1">2</div>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            <button className="border w-50 border-primary-wooden text-primary-wooden p-2 rounded-sm">
+              View Products
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductDisplay;
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,18 +1,8 @@
 import React from "react";
 import royal_Logo from "../assets/royal.png";
 import banner from "../assets/banner.jpg";
-import prod_1 from "../assets/prod_1.jpeg";
-import prod_2 from "../assets/prod_2.jpeg";
-import prod_3 from "../assets/prod_3.jpeg";
-import prod_4 from "../assets/prod_4.jpeg";
 import ContentLanding from "../components/contentBox";
-
-const obj = {
-  1: prod_1,
-  2: prod_2,
-  3: prod_3,
-  4: prod_4,
-};
+import ProductDisplay from "../components/product";
 
 const LandingPage = () => {
   return (
@@ -56,33 +46,7 @@ const LandingPage = () => {
         </div>
       </div>
       <ContentLanding />
-      <div className="p-10 h-full">
-        <h2 className=" font-bold text-2xl pb-2">Products</h2>
-        <div className="border border-primary-wooden w-full h-full rounded-2xl flex flex-col items-center gap-2 pb-4">
-          <div className="grid grid-cols-4 gap-2 w-full h-auto p-2 ">
-            {[1, 2, 3, 4]?.map((i) => {
-              const url = `prod_${i}`;
-              return (
-                <div className="product-card">
-                  <div className="flex-2">
-                    <img
-                      src={obj[i]}
-                      alt={`prod_${i}`}
-                      className="aspect-video w-full h-75 rounded-b-2xl"
-                    />
-                  </div>
-                  <div className="flex-1">2</div>
-                </div>
-              );
-            })}
-          </div>
-          <div>
-            <button className="border w-50 border-primary-wooden text-primary-wooden p-2 rounded-sm">
-              View Products
-            </button>
-          </div>
-        </div>
-      </div>
+      <ProductDisplay />
       <footer>footer</footer>
     </div>
   );
