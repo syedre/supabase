b70f09f82d5c85959b0fbca9a7c50fd47d27545f
syedre <syed.rehan123@gmail.com>
2026-06-17 22:32:35 +0530

    fixed product grid

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -16,7 +16,7 @@ const ProductDisplay = () => {
       <div className="p-10 h-full">
         <h2 className=" text-2xl pb-2">Products</h2>
         <div className="border border-light-wood w-full h-full rounded-2xl flex flex-col items-center gap-2 pb-4">
-          <div className="grid grid-cols-4 gap-2 w-full h-auto p-2 ">
+          <div className="grid md:grid-cols-4 sm:grid-cols-1 gap-2 w-full h-auto p-2 ">
             {[1, 2, 3, 4]?.map((i) => {
               const url = `prod_${i}`;
               return (
