9c37ea1bc0aab608abe7fe802f60a4f1f3a80c5a
syedre <syed.rehan123@gmail.com>
2026-06-18 09:56:15 +0530

    fixed content

--- src/components/contentBox.jsx
+++ src/components/contentBox.jsx
@@ -10,22 +10,27 @@ const data = [
 
 const ContentLanding = () => {
   return (
-    <div className="bg-[#F3EFE6] px-14 py-10">
+    <div className="bg-[#F3EFE6] px-14 py-10 flex flex-col">
       <h5 className="text-light-wood">WHERE HERITAGE MEETS ROYALTY</h5>
       <h2 className="text-4xl">Premium</h2>
       <h2 className="text-4xl">Ambari</h2>
       <h2 className="text-light-wood font-semibold text-4xl">Elephants</h2>
-      <p className="text-[#898F83] w-100 mt-4">
-        Handcrafted masterpieces inspired by the grandeur of Indian royal
-        processions and timeless cultural artistry. Each carving reflects
-        strength, prestige, tradition, and luxury.
-      </p>
-      <p className="text-[#898F83] w-100 mt-4 mb-4">
-        A perfect choice for VIP gifting, premium décor, and memorable
-        celebrations — finished in natural wood tones with intricate hand-relief
-        carving across every surface.
-      </p>
-      <div className="grid grid-cols-2 gap-2 mb-6 ">
+      <div className="grid  md:grid-cols-2 sm-grid-cols-1">
+        <div>
+          <p className="text-[#898F83] w-auto mt-4">
+            Handcrafted masterpieces inspired by the grandeur of Indian royal
+            processions and timeless cultural artistry. Each carving reflects
+            strength, prestige, tradition, and luxury.
+          </p>
+          <p className="text-[#898F83] w-auto mt-4 mb-4">
+            A perfect choice for VIP gifting, premium décor, and memorable
+            celebrations — finished in natural wood tones with intricate
+            hand-relief carving across every surface.
+          </p>
+        </div>
+      </div>
+
+      <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-2 mb-6 ">
         {data.map((i, index) => (
           <div
             key={index}
@@ -38,7 +43,7 @@ const ContentLanding = () => {
       <h4 className="text-[#898F83] text-sm tracking-widest font-semibold mt-4">
         AVAILABLE SIZES
       </h4>
-      <div className="flex items-center gap-2 mt-2">
+      <div className="flex items-center flex-wrap gap-2 mt-2">
         {sizes?.map((i, index) => (
           <button
             key={index}
