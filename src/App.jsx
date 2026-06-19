f93eb28010e09b111b29dac4c731415e8ee91f32
syedre <syed.rehan123@gmail.com>
2026-06-19 16:02:59 +0530

    cleaned up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,15 @@
-import { createContext, useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import ExpandFolder from "./components/fileExplorer";
-import Tabs from "./components/compoundComponent";
-import CustomSelect from "./components/selectCom";
-import Inter from "./components/intersection";
-import CustomSwitch from "./components/customSwitch";
-import Otp from "./components/otp";
-import MultiStep from "./components/multistep";
-import Todo from "./components/todo";
-import DebouncedSearch from "./components/debounceSearch";
-import Supa from "./components/supabase";
-import Signup from "./components/signUp";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "./components/login";
-import ProtectedRoute from "./components/protectedRoute";
-import AuthLayout from "./components/authLayout";
 import LandingPage from "./pages/landingPage";
 import ProductCategory from "./pages/productCategoryPage";
 import DashboardPage from "./pages/dashboardPage";
 
-export const UserContext = createContext();
 function App() {
-  const [isDark, setIsDark] = useState(false);
-
-  useEffect(() => {
-    document.documentElement.classList.toggle(
-      "dark",
-      isDark === true ? true : false,
-    );
-  }, [isDark]);
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <AuthLayout>
-              <Signup />
-            </AuthLayout>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <AuthLayout>
-              <Login />
-            </AuthLayout>
-          }
-        />
-        <Route
-          path="/todo"
-          element={
-            <ProtectedRoute>
-              <Todo />
-            </ProtectedRoute>
-          }
-        />
         <Route path="/landing" element={<LandingPage />} />
         <Route path="/products" element={<ProductCategory />} />
         <Route path="/dashboard" element={<DashboardPage />} />
--- src/header/navHeader.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const NavHeader = () => {
-  return <div></div>;
-};
